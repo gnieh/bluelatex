ed5479e01faede5d535cd48e2cc904f9bd35ed4c
Thomas Durieux <durieuxthomas@hotmail.com>
2014-04-21 19:18:20 +0200

    Improve the header of the site

--- blue-web/web_client/js/menu.js
+++ blue-web/web_client/js/menu.js
@@ -1,54 +1,89 @@
 angular.module('bluelatex.menu', [])
   .controller('menuController', function($scope,$route) {
+    $scope.$on( "$routeChangeSuccess", function(event, route) {
+      console.log(route);
+      $scope.menus = [];
+      for (var i = 0; i < defaulfMenu.length; i++) {
+        defaulfMenu[i].active = route.$$route.options.name == defaulfMenu[i].name;
+        $scope.menus.push(defaulfMenu[i]);
+      }
+      $scope.options = [];
+      var _options = optionsPage[route.$$route.options.name];
+      if(_options!=null)
+      for (var i = 0; i < _options.length; i++) {
+        $scope.options.push(_options[i]);
+      }
+    });
     var defaulfMenu = [
+      {
+        label: 'Papers',
+        link: '#/papers',
+        name: 'papers',
+        connected: true,
+        unconnected: false,
+        active: false
+      },
       {
         label: 'Login',
         link: '#/login',
-        name: 'login'
+        name: 'login',
+        connected: false,
+        unconnected: true,
+        active: false
       },
       {
         label: 'Register',
         link: '#/register',
-        name: 'register'
-      },
-      {
-        label: 'Papers',
-        link: '#/papers',
-        name: 'papers'
-      },
-      {
-        label: 'Profil',
-        link: '#/profil.html',
-        name: 'profil'
+        name: 'register',
+        connected: false,
+        unconnected: true,
+        active: false
       }
     ];
-    var options = [];
-    var menus = [];
+    var optionsPage = {
+      'papers': [
+        {
+          label: 'Add Paper',
+          type: 'action',
+          action: 'add_paper',
+          class: '',
+          icon: null
+        }
+      ],
+      'paper': [
+        {
+          label: 'Share',
+          type: 'menu',
+          class: '',
+          icon: null,
+          action: 'partials/paper_share_menu.html'
+        },
+        {
+          label: 'Settings',
+          type: 'menu',
+          class: '',
+          icon: null,
+          action: 'partials/paper_settings.html'
+        }
+      ]
+    };
 
-    for (var i = 0; i < defaulfMenu.length; i++) {
-      options.push(defaulfMenu[i]);
-    };
-    $scope.menus = menus;
-    $scope.options = options;
+    $scope.menus = [];
+    $scope.options = [];
 
-    $scope.addMenu = function (m) {
-      menus.push(m);
-    };
-    $scope.addOption = function (o) {
-      options.push(o);
-    };
-    $scope.addOptions = function (o) {
-      for (var i = 0; i < o.length; i++) {
-        options.push(o[i]);
-      };
-    };
+    $scope.action = function (option) {
+      console.log(option);
+
+      if(option.type == 'action') {
+        $scope.$emit('handleTopAction', option.action);
+      } else if(option.type == 'menu') {
+        option.display = !option.display;
+      }
+      return false;
+    }
   })
   .directive('blMenu', function() {
     return {
       templateUrl: 'partials/menu.html'
-    };
-  }).directive('blOptions', function() {
-    return {
-      templateUrl: 'partials/options.html'
     };
   });
