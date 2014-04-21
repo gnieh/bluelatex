02d7dbc2d410b463d15cd7ac14ff356920dee95f
Thomas Durieux <durieuxthomas@hotmail.com>
2014-04-21 19:25:19 +0200

    Update the script used to update angularjs
    Correct the path of the project

--- blue-web/update-angular.sh
+++ blue-web/update-angular.sh
@@ -2,14 +2,14 @@
 if [ -n "$1" ]; then
   mkdir tmp
   curl https://raw.github.com/angular/code.angularjs.org/master/$1/angular-$1.zip -o tmp/angular.zip
-  rm -fr app/lib/angular
-  unzip tmp/angular.zip -d app/lib
-  mv app/lib/angular-$1 app/lib/angular
-  rm -fr app/lib/angular/docs
-  mv app/lib/angular/angular-mocks.js test/lib/angular
-  mv app/lib/angular/angular-scenario.js test/lib/angular
-  cp app/lib/angular/version.txt test/lib/angular
+  rm -fr web_client/lib/angular
+  unzip tmp/angular.zip -d web_client/lib
+  mv web_client/lib/angular-$1 web_client/lib/angular
+  rm -fr web_client/lib/angular/docs
+  mv web_client/lib/angular/angular-mocks.js test/lib/angular
+  mv web_client/lib/angular/angular-scenario.js test/lib/angular
+  cp web_client/lib/angular/version.txt test/lib/angular
 
 else
   echo "Usage: update-angular <version>"
-fi  
+fi
