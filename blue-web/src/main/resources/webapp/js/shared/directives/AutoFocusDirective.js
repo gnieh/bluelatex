243a3dde0da17732ae5651fa8d4491f9197ee280
Thomas Durieux <durieuxthomas@hotmail.com>
2014-06-16 11:08:34 +0200

    Rename autofocus directive

--- blue-web/src/main/resources/webapp/js/shared/directives/AutoFocusDirective.js
+++ blue-web/src/main/resources/webapp/js/shared/directives/AutoFocusDirective.js
@@ -1,7 +1,7 @@
 'use strict';
 // A directive witch give the focus on an element
 angular.module('bluelatex.Shared.Directives.Autofocus', [])
-  .directive('blAutofocus', function() {
+  .directive('autofocus', function() {
      return function(scope, elem, attr) {
         elem[0].focus();
      };
