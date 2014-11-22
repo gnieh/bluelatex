49051629945b0f0e1a692a5b76e649b185d080d1
Lucas Satabin <lucas.satabin@gnieh.org>
2014-11-22 11:58:17 +0100

    Remove dependency to scalate
    scalate is really good as a template engine for things like jsp or such.
    In our case, we do not generate html pages dynamically, only paper
    templates and emails are loaded as templates.
    Using our own lightweight mustache template engine we avoid having a
    resident scala compiler, we avoid clsasloader problems with OSGi, the
    warmup phase is much smaller, and it was fun and easy to implement.
    
    Moreover this makes less dependencies, once we do not use lift-json
    anymore, no dependency on the scala compiler will be required anymore,
    and that will be a great step.

--- blue-common/src/main/scala/gnieh/blue/common/impl/TemplatesImpl.scala
+++ blue-common/src/main/scala/gnieh/blue/common/impl/TemplatesImpl.scala
@@ -17,41 +17,14 @@ package gnieh.blue
 package common
 package impl
 
-import org.fusesource.scalate._
-import org.fusesource.scalate.support._
-import org.fusesource.scalate.util._
-
-import java.io.{
-  File,
-  FileNotFoundException
-}
+import gnieh.mustache._
 
 class TemplatesImpl(configuration: BlueConfiguration) extends Templates {
 
-  import FileUtils._
+  val engine =
+    new MustacheProcessor(new FileSystemLoader(configuration.templateDir), true)
 
-  val engine = {
-    val engine = new TemplateEngine
-    engine.templateDirectories = List(configuration.templateDir.getCanonicalPath)
-    engine.resourceLoader = new FileResourceLoader {
-      override def resource(uri: String) =
-        super.resource(new File(configuration.templateDir, uri).getCanonicalPath)
-    }
-    engine
-  }
-
-  // used to set the correct classloader
-  private val scope = org.fusesource.scalate.mustache.Scope
-
-  def layout(name: String, params: (String, Any)*) = {
-    val previousCl = Thread.currentThread.getContextClassLoader
-    try {
-      Thread.currentThread.setContextClassLoader(scope.getClass.getClassLoader)
-      engine.layout(s"$name.mustache", Map(params: _*))
-    } finally {
-      Thread.currentThread.setContextClassLoader(previousCl)
-    }
-  }
+  def layout(name: String, params: (String, Any)*) =
+    engine.render(s"$name.mustache", Map(params: _*))
 
 }
-
